9dbbbb679ab1d061dcdcf63c0cde1b7f3a6b019b
B533P-03\533 <marcomw214@gmail.com>
2018-08-24 16:41:35 -0400

    AJOUTER UN COMPORTEMENT À LA FUSÉE

---
+++ Rocket/Assets/Script/Rocket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public Rigidbody rocketRigidBody;
+    [SerializeField] public float ForceFoward = 500;
+	// Use this for initialization
+	void Start () {
+        rocketRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Jump") > 0)
+        {
+            rocketRigidBody.AddForce(new Vector3(0, ForceFoward, 0));
+        }
+	}
+}
