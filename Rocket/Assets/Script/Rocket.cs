c912df7060c2adfcfc113307b011f7ae09090f76
B533P-03\533 <marcomw214@gmail.com>
2018-08-24 17:01:08 -0400

    tourné la fusée 1

--- Rocket/Assets/Script/Rocket.cs
+++ Rocket/Assets/Script/Rocket.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     public Rigidbody rocketRigidBody;
-    [SerializeField] public float ForceFoward = 500;
+    public Rigidbody rocketBoosterRight;
+    public Rigidbody rocketBoosterLeft;
+    [SerializeField] public float ForceJump = 100;
 	// Use this for initialization
 	void Start () {
         rocketRigidBody = GetComponent<Rigidbody>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetAxis("Jump") > 0)
         {
-            rocketRigidBody.AddForce(new Vector3(0, ForceFoward, 0));
+            rocketRigidBody.AddForce(new Vector3(0, ForceJump, 0));
+        }
+
+        if(Input.GetAxis("Horizontal") > 0 )
+        {
+            rocketRigidBody.AddForceAtPosition(new Vector3(0, 0, 10), new Vector3(0, 0 ,0));
+        }
+        else if(Input.GetAxis("Horizontal") < 0 )
+        {
+            rocketRigidBody.AddForceAtPosition(new Vector3(0, 0, 10) , new Vector3(0, 0, 0));
         }
 	}
 }
